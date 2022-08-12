ce7d1655746cdd8306633f732cdf2e8638b79bea
Hattinger04 <s.8greiderer@gmail.com>
2022-08-12 20:26:16 +0200

    now correct file

--- stop.bat
+++ stop.bat
@@ -1,11 +1,3 @@
 @ECHO OFF
-REM Start Nginx
-tasklist /FI "IMAGENAME eq nginx.exe" 2>NUL | find /I /N "nginx.exe">NUL
-IF NOT "%ERRORLEVEL%"=="0" (
-   REM Nginx is NOT running, so start it
-   cd nginx
-   start nginx.exe
-   ECHO Nginx started.
-) else (
-   ECHO Nginx is already running.
-)
+cd nginx
+taskkill /f /IM nginx.exe
